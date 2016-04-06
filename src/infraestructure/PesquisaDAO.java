1487ade7f66fc16e25fcd053a51b0e763dd6f5a1
Estevam <Estevam@192.168.0.147>
2016-04-06 10:54:56 -0300

    Não estou conseguindo executar na minha máquina. Irei dar o push e depois arrumar.

---
+++ src/infraestructure/PesquisaDAO.java
@@ -0,0 +1,44 @@
+package infraestructure;
+import entity.LivroEntity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.AutorEntity;
+import entity.CategoriaEntity;
+import entity.EditoraEntity;
+
+
+public class PesquisaDAO {
+	
+	public List<LivroEntity> perquisarLivro(String tituloLivro, String autorLivro, Double precoLivro) {
+		String query = "SELECT (`Titulo`,`PrecoVenda`,`CodAutor`) FROM `Livro` WHERE  like ?";
+		List<LivroEntity> lista = new ArrayList<LivroEntity>();
+		try {
+			Connection con = JDBCUtil.getConnection();
+
+			PreparedStatement stmt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1,  "%" + tituloLivro + "%" );
+			stmt.setString(2,  "%" + autorLivro + "%" );
+			stmt.setDouble(3,  "%" + precoLivro + "%" );
+			ResultSet rs  = stmt.executeQuery();
+			while( rs.next() ) { 
+				LivroEntity l = new LivroEntity();
+				l.setTituloLivro(rs.getString("tituloLivro"));
+				l.setNomeAutor(rs.getString("nomeAutor"));
+				l.setPrecoVenda(rs.getDouble("precoLivro"));
+				lista.add( l );
+			}
+		} catch (SQLException e) {
+			throw new Exception( e );
+		}
+		return lista;
+	}
+
+}
+
