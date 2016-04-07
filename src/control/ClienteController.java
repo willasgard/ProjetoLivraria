47e646693aadf87ad3c19c6e50e5c09dafef4e1c
William de Araujo <estudoswill@gmail.com>
2016-04-07 11:55:37 -0300

    atualizacao de seguranca

--- src/control/ClienteController.java
+++ src/control/ClienteController.java
@@ -6,11 +6,20 @@ import java.util.List;
 
 import javax.swing.JOptionPane;
 
+import javax.swing.JTextField;
+
 import entity.ClienteEntity;
 import infraestructure.CadastroClienteDAO;
 
 public class ClienteController implements iClientesController{
+private JTextField CodCliente;
 
+		public ClienteController(JTextField codClt){
+			this.CodCliente=codClt;
+		
+	}
+	
+	
 	private ArrayList<ClienteEntity> clt = new ArrayList<>();
 	
 	
@@ -19,6 +28,7 @@ public class ClienteController implements iClientesController{
 		
 		try {
 			CadastroClienteDAO cltDao  = new CadastroClienteDAO();
+			CodCliente.setText(String.valueOf(cltDao.proximoId()));
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			JOptionPane.showMessageDialog(null,e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
@@ -29,9 +39,20 @@ public class ClienteController implements iClientesController{
 
 	@Override
 	public void listaCliente() {
+		String r = null;
 		try {
 			CadastroClienteDAO cltDao  = new CadastroClienteDAO();
+			
 			List<ClienteEntity> listaCliente = cltDao.ConsultaCliente();
+			if(listaCliente != null){
+				for (ClienteEntity clt : listaCliente){
+					
+					
+					r = r +" " + clt;
+					System.out.println(r);
+				}
+			}
+			
 		} catch (SQLException e) {
 		
 			JOptionPane.showMessageDialog(null,e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
---
+++ src/control/EvBtnCliente.java
@@ -0,0 +1,44 @@
+package control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class EvBtnCliente implements ActionListener {
+		
+
+	JLabel lblNome, lblCPF, lblRG, lblCEP, lblLogradouro, lblNumero, lblComplemento, lblBairro, lblUF, lblCidade, lblEmail, lblTelefone, lblCelular;
+	
+	
+	
+	JTextField nome;
+	JTextField cpf;
+	JTextField RG;
+	JTextField CEP;
+	JTextField logradouro;
+	JTextField Numero;
+	JTextField Complemento;
+	JTextField Bairro;
+	JComboBox cbUF;
+	JTextField Cidade;
+	JTextField Email;
+	JTextField Telefone;
+	JTextField Celular;
+	
+	JButton btnCadastrar;
+	
+	
+
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
